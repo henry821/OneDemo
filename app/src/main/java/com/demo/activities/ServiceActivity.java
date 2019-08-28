5ba7eaed8e988a47bebb0374edf0feb31d793cce
wanghengwei <574926365@qq.com>
2019-08-28 20:21:07 +0800

    补充Service实例

--- app/src/main/java/com/demo/activities/ServiceActivity.java
+++ app/src/main/java/com/demo/activities/ServiceActivity.java
@@ -1,18 +1,26 @@
 package com.demo.activities;
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import com.baselibrary.utils.LogUtil;
 import com.demo.one.R;
+import com.demo.service.BindModeService;
 import com.demo.service.StartModeService;
 
 public class ServiceActivity extends AppCompatActivity {
 
     private Button btnStart;
     private Button btnBind;
+
+    private ServiceConnection mConnection;
+    private BindModeService.MyBinder mBinder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +30,32 @@ public class ServiceActivity extends AppCompatActivity {
         btnStart = findViewById(R.id.btn_start);
         btnBind = findViewById(R.id.btn_bind);
 
+        mConnection = new ServiceConnection() {
+            @Override
+            public void onServiceConnected(ComponentName name, IBinder service) {
+                LogUtil.e(getClass().getName() + "与" + name.getClass().getName() + "建立了连接");
+                mBinder = (BindModeService.MyBinder) service;
+                mBinder.execute();
+            }
+
+            @Override
+            public void onServiceDisconnected(ComponentName name) {
+                LogUtil.e(getClass().getName() + "与" + name.getClass().getName() + "断开了连接");
+            }
+        };
+
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startService(new Intent(ServiceActivity.this, StartModeService.class));
+            }
+        });
+
+        btnBind.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bindService(new Intent(ServiceActivity.this, BindModeService.class)
+                        , mConnection, BIND_AUTO_CREATE);
             }
         });
     }
@@ -33,6 +63,7 @@ public class ServiceActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         stopService(new Intent(ServiceActivity.this, StartModeService.class));
+        unbindService(mConnection);
         super.onDestroy();
 
     }
---
+++ app/src/main/java/com/demo/service/BindModeService.java
@@ -0,0 +1,34 @@
+package com.demo.service;
+
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+
+import com.baselibrary.utils.LogUtil;
+
+/**
+ * Description 使用start方法创建的Service
+ * Author wanghengwei
+ * Date   2019/8/28 19:29
+ */
+public class BindModeService extends BaseService {
+
+    private MyBinder myBinder = new MyBinder();
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        LogUtil.e(getClass().getName() + "--开始执行耗时操作--" + Thread.currentThread().getName());
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    IBinder getBinder(Intent intent) {
+        return myBinder;
+    }
+
+    public class MyBinder extends Binder {
+        public void execute() {
+            LogUtil.e(getClass().getName() + "--开始执行耗时操作--" + Thread.currentThread().getName());
+        }
+    }
+}

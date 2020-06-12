c247717e203e76a7df0d05ba56846552fbd32039
Henry <574926365@qq.com>
2020-06-12 21:02:18 +0800

    开始创建 太阳直播app Demo

---
+++ sunlive/src/main/java/com/sunlive/profile/ProfileActivity.java
@@ -0,0 +1,17 @@
+package com.sunlive.profile;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+import com.sunlive.BaseActivity;
+import com.sunlive.R;
+
+public class ProfileActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
---
+++ sunlive/src/main/java/com/sunlive/video/VideoActivity.java
@@ -0,0 +1,17 @@
+package com.sunlive.video;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+import com.sunlive.BaseActivity;
+import com.sunlive.R;
+
+public class VideoActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video);
+    }
+}

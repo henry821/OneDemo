c106ae8a98164b1e9b03dddccf8d4757c8e06982
wanghengwei <574926365@qq.com>
2021-03-08 17:46:39 +0800

    AGP可调试，加入获取屏幕信息工具类

---
+++ app/src/main/java/com/demo/utils/ScreenUtil.java
@@ -0,0 +1,15 @@
+package com.demo.utils;
+
+import android.app.Activity;
+
+public class ScreenUtil {
+
+    public static int getScreenWidth(Activity activity) {
+        return activity.getWindowManager().getDefaultDisplay().getWidth();
+    }
+
+    public static int getScreenHeight(Activity activity) {
+        return activity.getWindowManager().getDefaultDisplay().getHeight();
+    }
+
+}

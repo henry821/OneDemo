d383892b9bdb9b64fd5df477fb5cc75de730db01
wanghengwei <574926365@qq.com>
2021-03-16 11:40:09 +0800

    1.加入buildSrc模块 2.build.gradle切换成kotlin

---
+++ buildSrc/src/main/java/Dependency.kt
@@ -0,0 +1,19 @@
+/**
+ * Created by hengwei on 2021/3/16.
+ */
+object Version {
+    const val kotlin = "1.4.21"
+    const val appcompat = "1.2.0"
+    const val constraintlayout = "2.0.4"
+    const val core_ktx = "1.3.2"
+    const val recyclerview = "1.1.0"
+    const val lottie = "3.6.1"
+}
+
+object Dependency {
+    const val appcompat = "androidx.appcompat:appcompat:${Version.appcompat}"
+    const val constraintlayout = "androidx.constraintlayout:constraintlayout:${Version.constraintlayout}"
+    const val core_ktx = "androidx.core:core-ktx:${Version.core_ktx}"
+    const val recyclerview = "androidx.recyclerview:recyclerview:${Version.recyclerview}"
+    const val lottie = "com.airbnb.android:lottie:${Version.lottie}"
+}
---
+++ buildSrc/src/main/java/ProjectConfig.kt
@@ -0,0 +1,11 @@
+/**
+ * Created by hengwei on 2021/3/16.
+ */
+object ProjectConfig {
+    const val compileSdkVersion = 28
+    const val applicationId = "com.demo.one"
+    const val minSdkVersion = 16
+    const val targetSdkVersion = 28
+    const val versionCode = 1
+    const val versionName = "1.0"
+}

86eacc72a9c8772db8dd4e13dac9d67ed6218d51
wanghengwei <574926365@qq.com>
2021-05-11 13:08:54 +0800

    添加Google示例：ActionOpenDocumentTree

--- app/src/main/java/com/demo/google/storage/StorageEntranceActivity.kt
+++ app/src/main/java/com/demo/google/storage/StorageEntranceActivity.kt
@@ -1,7 +1,6 @@
 package com.demo.google.storage
 
 import com.demo.activity.EntranceActivity
-import com.demo.google.storage.actionopendocument.MainActivity
 import com.demo.one.R
 
 /**
@@ -11,7 +10,10 @@ class StorageEntranceActivity : EntranceActivity() {
 
     override fun initData(): List<MainData> {
         val list = ArrayList<MainData>()
-        list.add(MainData(getString(R.string.action_open_document_app_name), MainActivity::class.java))
+        list.add(MainData(getString(R.string.action_open_document_app_name),
+            com.demo.google.storage.actionopendocument.MainActivity::class.java))
+        list.add(MainData(getString(R.string.action_open_document_tree_app_name),
+            com.demo.google.storage.actionopendocumenttree.MainActivity::class.java))
         return list
     }
 

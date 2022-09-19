cbf85fd3cd381aed64138b5f23168453befd4252
wanghengwei <574926365@qq.com>
2022-09-19 14:29:20 +0800

    协程

--- app/src/main/java/com/demo/utils/Utils.kt
+++ app/src/main/java/com/demo/utils/Utils.kt
@@ -16,3 +16,12 @@ inline val Float.dp: Float
 
 inline val Int.dp: Int
     get() = this.toFloat().dp.toInt()
+
+inline val Int.screenWidth: Int
+    get() = Resources.getSystem().displayMetrics.widthPixels
+
+inline val Int.screenHeight: Int
+    get() = Resources.getSystem().displayMetrics.heightPixels
+
+inline val String.appendThreadInfo: String
+    get() = "$this -- ${Thread.currentThread().name}"

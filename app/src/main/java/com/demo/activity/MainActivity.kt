17893f09787fba8605495bb1336f16773cffbba5
wanghengwei <574926365@qq.com>
2022-11-15 11:55:52 +0800

    migrate to viewbinding

--- app/src/main/java/com/demo/activity/MainActivity.kt
+++ app/src/main/java/com/demo/activity/MainActivity.kt
@@ -3,30 +3,30 @@ package com.demo.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.navigation.findNavController
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.demo.one.R
+import com.demo.one.databinding.ActivityMainBinding
 import com.demo.widgets.OverviewFloatView
-import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    private val drawerLayout: DrawerLayout by lazy { findViewById(R.id.drawer) }
+    private lateinit var binding: ActivityMainBinding
     private lateinit var overviewFloatView: OverviewFloatView
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         WindowCompat.setDecorFitsSystemWindows(window, false)
-        setContentView(R.layout.activity_main)
 
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
-        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
-        val navigationView: NavigationView = findViewById(R.id.navigation_view)
-        NavigationUI.setupWithNavController(navigationView, navController)
+        NavigationUI.setupActionBarWithNavController(this, navController, binding.root)
+        NavigationUI.setupWithNavController(binding.navigationView, navController)
     }
 
     override fun onAttachedToWindow() {
@@ -36,8 +36,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        val navController = this.findNavController(R.id.nav_host_fragment)
-        return NavigationUI.navigateUp(navController, drawerLayout)
+        return NavigationUI.navigateUp(navController, binding.root)
     }
 
 }
